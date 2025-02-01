404de0826ef6fc2aea2360a8fb17f44c77064596
megala3105 <megalagajanand31@gmail.com>
2025-02-01 22:54:31 +0530

    Add files via upload
    commit assignments

---
+++ LearnDataTypes.java
@@ -0,0 +1,50 @@
+package week1.day1;
+
+public class LearnDataTypes {
+
+	public static void main(String[] args) {
+		 //integer
+		//datatype variablename =value
+		//byte datatype
+		
+		byte age=36;
+		
+		//short
+		
+		short ageofpersons=193;
+		
+		
+		//integer
+		
+		int amount=500000;
+		
+		//long
+		long mobileno=9632574128l;
+		
+		//Float
+		
+		float rateofinterest=1.30f;
+		System.out.println(rateofinterest);
+		
+		
+		//double
+		double amountpaid=1000.35;
+		
+		//character
+		char name='K';
+		
+		//boolean
+		boolean weekend=true;
+		
+		System.out.println("Primitive datatypes");
+		
+		//Strings is a collection of characters Megala
+		//string is a class in java so it is mentioned in upper class
+		
+		String myname="Megala";
+		System.out.println("My name is "+myname);
+		
+
+	}
+
+}
