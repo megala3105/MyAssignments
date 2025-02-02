829abf57300aa8e46d9c996a6efd152cf14d17c0
megala3105 <megalagajanand31@gmail.com>
2025-02-02 12:25:18 +0530

    Add files via upload
    switch case

---
+++ LearnSwitchCase.java
@@ -0,0 +1,22 @@
+package week1.day2;
+
+public class LearnSwitchCase {
+
+	public static void main(String[] args) {
+		//switch case
+		int num=-1;
+		switch(num)
+		{
+		case 0:
+			System.out.println("The number is zero");
+			break;
+		case 1:
+			System.out.println("The number is 1");
+			break;
+			default:
+				System.out.println("The number is negative");
+				
+	}
+
+}
+}
